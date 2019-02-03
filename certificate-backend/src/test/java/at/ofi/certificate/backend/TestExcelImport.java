a517a90118699209d886d499a54e2b927c8e8642
hfrebort <harald@frebort.at>
2019-02-03 23:10:35 +0100

    add poi-ooxml depnedency
    - add ooxml dependency
    - rename excel sheet reader
    - change return value of reader

--- certificate-backend/src/test/java/at/ofi/certificate/backend/TestExcelImport.java
+++ certificate-backend/src/test/java/at/ofi/certificate/backend/TestExcelImport.java
@@ -1,66 +1,57 @@
 package at.ofi.certificate.backend;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.stream.Stream;
 
-import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Assert;
 import org.junit.Test;
-
 
 /**
  * @author ce-harald.frebort
  */
 public class TestExcelImport {
 
-    @Test
-    public void readSheet() throws Exception {
-    	
-    	try (Workbook wb = WorkbookFactory.create(new File ("c:\\temp\\Auditprogramm Muster.xlsx"))) {
-    		Sheet sheet = wb.getSheet("Auditprogramm - Gültige Zert");
-    		Stream<List<String>> data =  at.ofi.certificate.backend.excel.readsheet(sheet);
-    		assertTrue( data.count() > 0 );
-    	}
-    }
-    
-    @Test
-    public void writeSheetToFile() throws Exception {
-    	
-    	try (Workbook wb = WorkbookFactory.create(new File ("c:\\temp\\Auditprogramm Muster.xlsx"));
- 	    	 PrintWriter writer = getUTF8writer("c:\\temp\\JavaXls\\out1.UTF8.txt")) 
-    	{
-    		Sheet sheet = wb.getSheet("Auditprogramm - Gültige Zert");		
-    		at.ofi.certificate.backend.excel.readsheet(sheet)
-	    	.forEach(row -> {
-	    		//writer.println(String.join(";", row));
-	    		writer.printf("%d|%s\n", row.size(), String.join(";", row));
-	    	});
-    	}
-    }
-    
-    private static PrintWriter getUTF8writer(String filename) throws FileNotFoundException
-    {
-    	return 
-			new PrintWriter(
-   				new BufferedWriter(
-   					new OutputStreamWriter(
-						new FileOutputStream(filename), 
-						StandardCharsets.UTF_8)));
-	    		
-    }
+	private static final String FILE_NAME = "c:\\temp\\Auditprogramm Muster.xlsx";
+
+	@Test
+	public void readSheet() throws Exception {
+		File file = new File(FILE_NAME);
+
+		try (Workbook wb = WorkbookFactory.create(file)) {
+			Sheet sheet = wb.getSheet("Auditprogramm - Gültige Zert");
+			List<String> data = at.ofi.certificate.backend.ExcelSheetReader.readsheet(sheet);
+			assertTrue(data.size() > 0);
+		} catch (Exception e) {
+			Assert.fail("Can not load workbook");
+		}
+	}
+
+	@Test
+	public void writeSheetToFile() throws Exception {
+		try (Workbook wb = WorkbookFactory.create(new File(FILE_NAME));
+				PrintWriter writer = getUTF8writer("c:\\temp\\JavaXls\\out1.UTF8.txt")) {
+			Sheet sheet = wb.getSheet("Auditprogramm - Gültige Zert");
+			at.ofi.certificate.backend.ExcelSheetReader.readsheet(sheet).forEach(row -> {
+				// writer.println(String.join(";", row));
+				writer.printf("%d|%s\n", row, String.join(";", row));
+			});
+		}
+	}
+
+	private static PrintWriter getUTF8writer(String filename) throws FileNotFoundException {
+		return new PrintWriter(
+				new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), StandardCharsets.UTF_8)));
+
+	}
 }
